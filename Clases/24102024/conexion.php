82fbc59ed3c43e0bde89ed3791a8bf7021fb4124
SergioAdriel <sergiomunoz320@aragon.unam.mx>
2024-10-24 12:29:57 -0600

    Agregando clase del dia 24102024

---
+++ Clases/24102024/conexion.php
@@ -0,0 +1,14 @@
+<?php
+    $host_db="127.0.0.1:3307";
+    $user_name="root";
+    $user_pass="200477";
+    $db_name="fes_aragon";
+
+    $conexion = new mysqli($host_db,$user_name,$user_pass,$db_name);
+    
+    if($conexion->connect_error){
+      }
+      else{
+        echo "<h4 style='text-align: left;'>Bienvenido al sistema</h4>";
+      } 
+?>
---
+++ Clases/24102024/index.php
@@ -0,0 +1,45 @@
+<?php
+    include "conexion.php"; 
+    $consulta_sql = "SELECT * FROM alumno";
+    $resultado = $conexion->query($consulta_sql);
+    $count = mysqli_num_rows($resultado);
+ 
+echo "<table border='2'>
+        <tr style='background-color: #94F3FF;'>
+            <th>Usuario</th>
+            <th>No. de Cuenta Institucional</th>
+            <th>Dirección</th>
+            <th>Teléfono</th>
+            <th>Correo Electrónico</th>
+            <th>Contraseña</th>
+            <th>Fecha de Registro</th>
+            <th>Permisos</th>
+        </tr>";
+
+if ($count > 0) {
+     while ($row = mysqli_fetch_assoc($resultado)) {
+        echo "<tr style='background-color: #DFF4F7;'>";
+        echo "<td>".$row['nombre_usuario']."</td>";
+        echo "<td>".$row['no_cuenta']."</td>";
+        echo "<td>".$row['direccion']."</td>";
+        echo "<td>".$row['telefono']."</td>";
+        echo "<td>".$row['email']."</td>";
+        echo "<td>".$row['password']."</td>";
+        echo "<td>".$row['fecha_registro']."</td>";
+        echo "<td>".$row['permisos']."</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+
+}else{
+    
+    ?>
+    
+    <h1 style='color:red' >Sin Ningun registro</h1>
+<?php } ?> 
+    <h1><a href='EliminarUsuario.php'>ElimnarUsuario</a></h1>
+    <h1><a href='Registro.php'>Registro</a></h1>
+    
+
+
+
