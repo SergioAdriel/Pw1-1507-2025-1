69f7d41473937e92a2110df84b53e47229d468b5
SergioAdriel <sergiomunoz320@aragon.unam.mx>
2024-11-16 16:35:39 -0600

    Agregando Archivos corregidos del Proyecto Final

--- ProyectoFinal/Controlador/enviarRegistro.php
+++ ProyectoFinal/Controlador/enviarRegistro.php
@@ -1,33 +1,61 @@
 <?php
 include "./conexion.php";
-mysqli_set_charset($conexion,'utf8');
-$nombreUser=$_POST['nombre_usuario'];
+include "../header.php";
+mysqli_set_charset($conexion, 'utf8');
 
-$buscarusuario="SELECT * FROM  alumno WHERE nombre_usuario ='$nombreUser'";
+$nombreUser    = $_POST['nombre_usuario'];
+$noCuenta = $_POST['no_cuenta'];
 
-$resultado = $conexion -> query($buscarusuario);
-$count =mysqli_num_rows($resultado);
-if($count==1){
-    echo"El usuario ya esta registrado";
-    echo "<a href='../Registro.php'>Nuevo registro</a>";
+// Verificar si el nombre de usuario ya existe
+$buscarUsuario = "SELECT * FROM alumno WHERE nombre_usuario = '$nombreUser '";
+$resultadoUsuario = $conexion->query($buscarUsuario);
+$countUsuario = mysqli_num_rows($resultadoUsuario);
 
-}else{
-
-    mysqli_query($conexion,"INSERT INTO alumno(
-        nombre_usuario,carrera,no_cuenta,direccion,telefono,email,password)
-        VALUES(
-            '$_POST[nombre_usuario]',
-            '$_POST[carrera]',
-            '$_POST[no_cuenta]',
-            '$_POST[direccion]',
-            '$_POST[telefono]',
-            '$_POST[email]',
-            '$_POST[password]'
-        )");
-        echo "<br> <h1>Usuario creado con exito</h1>";
-        echo "<a href='../Registro.php'>Puedes generar un Nuevo registro</a>";
-        echo "<a href='../Principal.php'>Ver registros</a>";
-}
- 
+// Verificar si el número de cuenta ya existe
+$buscarCuenta = "SELECT * FROM alumno WHERE no_cuenta = '$noCuenta'";
+$resultadoCuenta = $conexion->query($buscarCuenta);
+$countCuenta = mysqli_num_rows($resultadoCuenta);
 
 ?>
+
+<div class="row">
+    <div class="col s12 m5 offset-m3">
+        <div class="card">
+            <div class="card-content center-align">
+
+<?php
+if ($countUsuario > 0) {
+    echo "<span class='card-title red-text text-darken-2'>Error de Registro</span>";
+    echo "<p>El nombre de usuario ya está registrado.</p>";
+    echo "<a href='../registro.php' class='btn waves-effect waves-light blue'>Volver al formulario</a>";
+} elseif ($countCuenta > 0) {
+    echo "<span class='card-title red-text text-darken-2'>Error de Registro</span>";
+    echo "<p>El número de cuenta ya está registrado.</p>";
+    echo "<a href='../registro.php' class='btn waves-effect waves-light blue'>Volver al formulario</a>";
+} else {
+    // Intentar insertar el nuevo registro
+    try {
+        $insertar = "INSERT INTO alumno (nombre_usuario, carrera, no_cuenta, direccion, telefono, email, password)
+                     VALUES ('$_POST[nombre_usuario]', '$_POST[carrera]', '$_POST[no_cuenta]', '$_POST[direccion]', '$_POST[telefono]', '$_POST[email]', '$_POST[password]')";
+        
+        if ($conexion->query($insertar) === TRUE) {
+            echo "<span class='card-title green-text text-darken-2'>¡Usuario registrado con éxito!</span>";
+            echo "<p>El usuario ha sido registrado correctamente.</p>";
+            echo "<a href='../Principal.php' class='btn waves-effect waves-light blue'>Ver registros</a>";
+        } else {
+            throw new Exception("Error al registrar el usuario: " . $conexion->error);
+        }
+    } catch (Exception $e) {
+        echo "<span class='card-title red-text text-darken-2'>Error de Registro</span>";
+        echo "<p>Error: " . $e->getMessage() . "</p>";
+        echo "<a href='../registro.php' class='btn waves-effect waves-light blue'>Volver al formulario</a>";
+    }
+}
+?>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include "../footer.php"; ?>
--- ProyectoFinal/IndexError.php
+++ ProyectoFinal/IndexError.php
@@ -1,28 +1,15 @@
 <?php include "./header.php"; ?>
-
-<?php
-// Verificar si hay un mensaje de error pasado en la URL
-$error_message = isset($_GET['error']) ? $_GET['error'] : '';
-?>
-
 <div class="row">
     <div class="col s12 m5 offset-m3">
         <div class="card">
             <div class="card-content">
-                <span class="card-title center-align teal-text text-darken-2">Error de Login</span>
-
-                <!-- Mostrar el mensaje de error si existe -->
-                <?php if ($error_message): ?>
-                    <div class="error-message" style="color: red; text-align: center; margin-bottom: 20px;">
-                        <strong><?php echo $error_message; ?></strong>
-                    </div>
-                <?php endif; ?>
-
-                <p>Vuelve a intentarlo con tus datos correctos.</p>
-                <a href="login.php" class="btn-large waves-effect waves-light teal lighten-1">Regresar al login</a>
+                <div class="center-align">
+                    <span class="card-title red-text text-darken-2">Error de Login</span>
+                    <p>Vuelve a intentarlo con tus datos correctos.</p>
+                    <a href="login.php" class="btn waves-effect waves-light blue">Regresar al login</a>
+                </div>
             </div>
         </div>
     </div>
 </div>
-
 <?php include "./footer.php"; ?>
